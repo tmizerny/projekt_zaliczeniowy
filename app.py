1b1612d820ca5ad40114c264d10ad91324d4f4db
Tobiasz Mizerny <tmizerny@edu.cdv.pl>
2024-05-02 18:20:27 +0200

    Dodano wyłączanie okienek, jeżeli dane nie są załadowane

--- app.py
+++ app.py
@@ -5,63 +5,77 @@ import panel as pn
 import matplotlib.pyplot as plt
 import folium
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', 10000)
-
 pn.extension()
 
-# df_wszystkie_stacje = pd.DataFrame(pobierz_dane(1))
-# df_wszystkie_stacje = df_wszystkie_stacje[['id','stationName']]
-#
-#
-#
-# data = pn.pane.DataFrame(df_wszystkie_stacje).servable()
-#
-# # wartośći najmniejsze i największe
-# number_1 = pn.indicators.Number(name='Wartość najmniejsza',font_size='32pt')
-# number_2 = pn.indicators.Number(name='Wartość największa',font_size='32pt')
+mapa_start = pn.pane.plot.Folium(folium.Map(location=[52.41579805, 16.931231975777052]))
 
 source_menu = pn.widgets.Select(description="Z jakiego źródła aplikacja ma pobrać dane?", name='Wybierz źródło danych',
                                 options=['Usługa REST', 'Baza danych'])
 
-station_menu_all = pn.widgets.Select(options=[stacja['stationName'] for stacja in pobierz_dane(1)])
+
+def zaladuj_dane(event):
+    if source_menu.value == 'Usługa REST':
+        station_menu_all.options = [stacja['stationName'] for stacja in pobierz_dane(1)]
+        station_menu_all.disabled = False
+        city_input.disabled = False
+        localization.disabled = False
+        distance.disabled = False
+        button_input.disabled = False
+        button_distance_input.disabled = False
+    else:
+        ...
+    # Załadowanie danych z bazy danych
+
+
+load_data_button = pn.widgets.Button(name='Załaduj dane')
+load_data_button.on_click(zaladuj_dane)
+
+station_menu_all = pn.widgets.Select(name='Wszystkie stacje pomiarowe ze źródła: ',disabled=True)
+
 city_input = pn.widgets.TextInput(
     name="Znajdź stację w konkretnej miejscowości: ",
     placeholder="Wprowadź nazwę miejscowości: ",
-)
-button_input = pn.widgets.Button(name='🔍')
+    disabled=True)
 
-station_menu_city = pn.widgets.Select(name='Znalezione stacje: ',
-                                      options=[stacja['stationName'] for stacja in pobierz_dane(1) if
-                                               city_input.value in stacja['stationName']])
+
+def zaladuj_stacje_miejscowosc(event):
+    station_menu_city.options = [stacja['stationName'] for stacja in pobierz_dane(1) if
+                                 city_input.value in stacja['stationName'].split(',')[0]]
+    station_menu_city.disabled = False
+
+
+button_input = pn.widgets.Button(name='Szukaj stacji dla konkretnej miejscowości 🔍', disabled=True)
+button_input.on_click(zaladuj_stacje_miejscowosc)
+
+station_menu_city = pn.widgets.Select(name='Znalezione stacje: ', disabled=True)
 
 localization = pn.widgets.TextInput(description='Wyszukaj stacje pomiarowe w podanym promieniu: ',
                                     name='Najbliższe stacje pomiarowe',
-                                    placeholder='Wprowadź nazwę lokalizacji: ')
-distance = pn.widgets.TextInput(placeholder='Wprowadź promień wyszukiwania [w km]: ')
-
-button_distance_input = pn.widgets.Button(name='🔍')
-
-mapa = pn.pane.plot.Folium(folium.Map(location=[52.41579805, 16.931231975777052], zoom_start=12))
+                                    placeholder='Wprowadź nazwę lokalizacji: ', disabled=True)
+distance = pn.widgets.TextInput(placeholder='Wprowadź promień wyszukiwania [w km]: ', disabled=True)
 
 
-def zaznacz_na_mapie(mapa, miejsce=None, promien=None, lista_stacji=pobierz_dane(1)):
+def zaznacz_na_mapie(miejsce=localization.value_input, promien=distance.value_input,
+                     lista_stacji=pobierz_dane(1)):
+
+    promien = float(promien)
     centrum, wynik_lista, lokalizacje = najblizsze_stacje_pomiarowe(miejsce, promien, lista_stacji)
+    mapa = pn.pane.plot.Folium(folium.Map(location=centrum))
     for miejsce, koordynaty in lokalizacje.items():
-        folium.Marker(koordynaty, popup=f'{miejsce}').add_to(mapa)
+        folium.Marker(koordynaty, popup=f'{miejsce}').add_to(mapa.object)
 
-    folium.Circle(centrum, radius=promien,fill=True, fill_opacity=0.3, fill_color='yellowgreen').add_to(mapa)
+    folium.Circle(centrum, radius=promien, fill=True, fill_opacity=0.3, fill_color='yellowgreen').add_to(mapa.object)
 
-    return mapa
+
+button_distance_input = pn.widgets.Button(name='Szukaj najbliższej stacji 🔍', disabled=True)
+button_distance_input.on_click(zaznacz_na_mapie)
 
 
 pn.template.FastListTemplate(
     title='Jakość powietrza w Polsce',
-    sidebar=[source_menu, station_menu_all, city_input, button_input, station_menu_city,
+    sidebar=[source_menu, load_data_button, station_menu_all, city_input, button_input, station_menu_city,
              localization, distance, button_distance_input],
-    main=mapa,
+    main=pn.Column(mapa_start, width=300),
     background_color='#dcf5d0',
     header_background=' #00A170',
     accent_base_color='white',
